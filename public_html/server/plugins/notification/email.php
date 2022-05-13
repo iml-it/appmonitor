293021fb0b81f58914550f909f963292a25cda8b
Hahn Axel (hahn) <axel.hahn@iml.unibe.ch>
2022-05-13 14:22:58 +0200

    use notfication plugins as class

--- public_html/server/plugins/notification/email.php
+++ public_html/server/plugins/notification/email.php
@@ -21,32 +21,34 @@
  * 2022-05-13 <axel.hahn@iml.unibe.ch>  created
  */
 
-/**
- * send email notification
- * @param  array  $aOptions  array of options
- */
-function send_email($aOptions){
+class emailNotification{
+    /**
+     * send email notification
+     * @param  array  $aOptions  array of options
+     */
+    static public function send($aOptions){
 
-    // ----- checks
-    if(!$aOptions['from']){
-        return false; // no from address
-    }
-
-    if(!count($aOptions['to'])){
-        return false; // no to adress in server config nor app metadata
-    }
-
-    // ----- send
-    mail(implode(";", $aOptions['to']),  
-        utf8_decode(html_entity_decode($aOptions['subject'])), 
-        utf8_decode(html_entity_decode($aOptions['message'])),
-        "From: " . $aOptions['from'] . "\r\n" 
-        . "Reply-To: " . $aOptions['from'] . "\r\n"
-
-        . "X-Priority: 1 (Highest)\r\n"
-        . "X-MSMail-Priority: High\r\n"
-        . "Importance: High\r\n"
-
-    );
-    return true;
+       // ----- checks
+       if(!$aOptions['from']){
+           return false; // no from address
+       }
+   
+       if(!count($aOptions['to'])){
+           return false; // no to adress in server config nor app metadata
+       }
+   
+       // ----- send
+       mail(implode(";", $aOptions['to']),  
+           utf8_decode(html_entity_decode($aOptions['subject'])), 
+           utf8_decode(html_entity_decode($aOptions['message'])),
+           "From: " . $aOptions['from'] . "\r\n" 
+           . "Reply-To: " . $aOptions['from'] . "\r\n"
+   
+           . "X-Priority: 1 (Highest)\r\n"
+           . "X-MSMail-Priority: High\r\n"
+           . "Importance: High\r\n"
+   
+       );
+       return true;
+   }
 }
--- public_html/server/plugins/notification/slack.php
+++ public_html/server/plugins/notification/slack.php
@@ -21,37 +21,39 @@
  * 2022-05-13 <axel.hahn@iml.unibe.ch>  created
  */
 
- /**
- * send slack notifications
- * @param  array  $aOptions  array of options
- */
-function send_slack($aOptions){
+class slackNotification{
+    /**
+     * send slack notifications
+     * @param  array  $aOptions  array of options
+     */
+    static public function send($aOptions){
 
-    // ----- checks
-    if(!is_array($aOptions['to']) || !count($aOptions['to'])){
-        return false; // no slack channel in server config nor app metadata
+        // ----- checks
+        if(!is_array($aOptions['to']) || !count($aOptions['to'])){
+            return false; // no slack channel in server config nor app metadata
+        }
+
+        // ----- send
+        $data=array(
+            'text'       => $aOptions['message'],
+            'username'   => '[APPMONITOR]',
+            'icon_emoji' => false
+        );
+
+        $options = array(
+            'http' => array(
+            'header'  => 'Content-type: application/x-www-form-urlencoded\r\n',
+            'method'  => 'POST',
+            'content' => json_encode($data)
+            )
+        );
+        $context  = stream_context_create($options);
+
+        // --- loop over slack targets
+        foreach($aOptions['to'] as $sLabel=>$sChannelUrl){
+            @file_get_contents($sChannelUrl, false, $context);
+        }
+
+        return true;
     }
-
-    // ----- send
-    $data=array(
-        'text'       => $aOptions['message'],
-        'username'   => '[APPMONITOR]',
-        'icon_emoji' => false
-    );
-
-    $options = array(
-        'http' => array(
-        'header'  => 'Content-type: application/x-www-form-urlencoded\r\n',
-        'method'  => 'POST',
-        'content' => json_encode($data)
-        )
-    );
-    $context  = stream_context_create($options);
-
-    // --- loop over slack targets
-    foreach($aOptions['to'] as $sLabel=>$sChannelUrl){
-        @file_get_contents($sChannelUrl, false, $context);
-    }
-
-    return true;
 }
