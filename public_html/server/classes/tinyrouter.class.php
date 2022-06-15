ad2c61aad855bd385c4ea9a4184a200ff8489d40
Hahn Axel (hahn) <axel.hahn@iml.unibe.ch>
2022-06-15 16:44:28 +0200

    WIP: tiny router class

---
+++ public_html/server/classes/tinyrouter.class.php
@@ -0,0 +1,146 @@
+<?php
+/**
+ * Axels first router
+ * 
+ */
+class tinyrouter{
+
+    public $sUrl = '';
+    public $aRoutes = [];
+    public $aMatch = false;
+
+    /**
+     * constructor
+     * @param  $aRoutes  array   array of routings
+     * @param  $sUrl     string  incoming url
+     */
+    public function __construct($aRoutes=[], $sUrl=false){
+        $this->setRoutes($aRoutes);
+        $this->setUrl($sUrl);
+    }
+
+    /**
+     * detect last matching route item
+     * if no route matches then it returns false
+     */
+    protected function _getRoute(){
+        $aReturn=[];
+        $this->aMatch=[];
+        if(!$this->sUrl || !count($this->aRoutes)){
+            return false;
+        }
+        $aReqParts=$this->getUrlParts();
+        foreach($this->aRoutes as $aRoutecfg){
+            $sRoute=$aRoutecfg[0];
+            $aParts=$this->getUrlParts($sRoute);
+            if (count($aParts) == count($aReqParts)){
+                $iPart=0;
+                $aVars=[];
+                $bFoundRoute=false;
+                foreach($aParts as $sPart){
+                    // detect @varname or @varname:regex in a routing
+                    if($sPart[0]=="@"){
+                        $sValue=$aReqParts[$iPart];
+                        preg_match('/\@([a-z]*):(.*)/', $sPart, $match);
+
+                        if(isset($match[2])){
+                            // if a given regex does not match the value then abort
+                            if(!preg_match("/^$match[2]$/", $sValue)){
+                                $bFoundRoute=false;
+                                break;
+                            }
+                        }
+                        // store a variable without starting @
+                        $aVars[$match[1]]=$sValue;
+                    } else {
+                        // no @ ... if string of url parts in url and route
+                        // do not match then we abort
+                        if($aParts[$iPart]!==$aReqParts[$iPart]){
+                            $bFoundRoute=false;
+                            break;
+                        }
+                    }
+                    $bFoundRoute=true;
+                    // $aVars[$iPart]=$sValue;
+                    $iPart++;
+                }
+                if($bFoundRoute){
+                    $aReturn=[
+                        "route"=>$aRoutecfg[0],
+                        "callback"=>$aRoutecfg[1],
+                        "vars"=>$aVars
+                    ];
+                }
+            }
+        }
+        $this->aMatch=count($aReturn) ? $aReturn : false;
+        return $this->aMatch;
+    }
+    
+
+    /**
+     * set routes
+     */
+    public function setRoutes($aRoutes=[]){
+        if(is_array($aRoutes) && count($aRoutes)){
+            $this->aRoutes=$aRoutes;
+            $this->_getRoute();
+        }
+        return true;
+    }
+
+    /**
+     * set incoming url
+     */
+    public function setUrl($sUrl){
+        $this->sUrl=$sUrl;
+        $this->_getRoute();
+    }
+
+    /**
+     * helper function: get url parts as array
+     * @returns array
+     */
+    public function getUrlParts($sUrl=false){
+        if(!$sUrl){
+            $sUrl=$this->sUrl;
+        }
+        $aReqParts=explode('/', $sUrl);
+        if ($sUrl[0]=='/'){
+            array_shift($aReqParts);
+        }
+        return $aReqParts;
+    }
+
+
+    /**
+     * detect last matching route item
+     * if no route matches then it returns false
+     * @return array|bool
+     */
+    public function getRoute(){
+        return $this->aMatch;
+    }
+
+    /**
+     * return the callback of matching route
+     * @return string
+     */
+    public function getCallback(){
+        return isset($this->aMatch['callback']) ? $this->aMatch['callback'] : false; 
+    }
+    /**
+     * return the variables as keys in route parts with starting @ character
+     * @return array
+     */
+    public function getVars(){
+        return isset($this->aMatch['vars']) ? $this->aMatch['vars'] : false; 
+    }
+    /**
+     * return the variables as keys in route parts with starting @ character
+     * @return array
+     */
+    public function getVar($sVarname){
+        return isset($this->aMatch['vars'][$sVarname]) ? $this->aMatch['vars'][$sVarname] : false; 
+    }
+}
---
+++ public_html/server/classes/tinyrouter.class.php.md
@@ -0,0 +1,89 @@
+# TinyRouter
+
+Axels first php class with a router.
+
+## Installation
+
+Copy the file *tinyrouter.class.php* somewhere into your project.
+
+## Example
+
+```php
+
+// load the class
+require_once('../classes/tinyrouter.class.php');
+
+// define routes
+$aRoutes=[
+    [ "/config",                             "get_config"     ],
+    [ "/config/@var",                        "get_config_var" ],
+
+    [ "/apps",                               "listapps"       ],
+    [ "/apps/@appid:[0-9a-f]*",              "acess_appdata"  ],
+    [ "/apps/@appid:[0-9a-f]*/@what:[a-z]*", "acess_appdata"  ],
+//    ^                                       ^
+//    |                                       |
+//    route                                   callback
+//      string     = folder
+//      @var       = set a variable
+//      @var:regex = set variable if it matches the given regex
+];
+
+// take an url ... or use the request uri if you have pretty urls
+$sApiUrl=isset($_GET['request']) && $_GET['request'] ? $_GET['request'] : false;
+
+// init the class
+$oRouter=new tinyrouter($aRoutes, $sApiUrl);
+
+// it is the same like
+// $oRouter=new tinyrouter();
+// $oRouter->setRoutes($aRoutes);
+// $oRouter->setUrl($sApiUrl);
+
+// get the last matching route
+$aFoundRoute=$oRouter->getRoute();
+if(!$aFoundRoute){
+    header('HTTP/1.0 400 Bad request');
+    die('<h1>400 Bad request</h1>');
+}
+
+// ... continue
+```
+
+The getRoute() method returns an array
+
+* with the matching route
+* name of the callback
+* vars on parts with starting @ chars
+
+```txt
+// on url /apps/12345/meta
+
+Array
+(
+    [route] => /apps/@appid:[0-9a-f]*/@what:[a-z]*
+    [callback] => acess_appdata
+    [vars] => Array
+        (
+            [appid] => 12345
+            [what] => meta
+        )
+
+)
+```
+
+If no route matches - or a variable did not match a required regex - then getRoute() returns *false*.
+
+Maybe the keys of the array change in future. You can access the data with specialized getter functions:
+
+```php
+// get the fallback 
+$sAction=$oRouter->getCallback();
+
+// all vars
+$aAllvars=$oRouter->getVars();
+
+// get single vars
+$sAppId=$oRouter->getVar('appid');
+$sWhat=$oRouter->getVar('what');
+```
