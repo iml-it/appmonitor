6192f14460d24b0467711f6c7c30b37bf85478e6
Hahn Axel (hahn) <axel.hahn@iml.unibe.ch>
2022-07-05 15:42:41 +0200

    added ping check (wip)

---
+++ public_html/client/plugins/checks/ping.php
@@ -0,0 +1,86 @@
+<?php
+/**
+ * ____________________________________________________________________________
+ * 
+ *  _____ _____ __                   _____         _ _           
+ * |     |     |  |      ___ ___ ___|     |___ ___|_| |_ ___ ___ 
+ * |-   -| | | |  |__   | .'| . | . | | | | . |   | |  _| . |  _|
+ * |_____|_|_|_|_____|  |__,|  _|  _|_|_|_|___|_|_|_|_| |___|_|  
+ *                          |_| |_|                              
+ *                           _ _         _                                            
+ *                       ___| |_|___ ___| |_                                          
+ *                      |  _| | | -_|   |  _|                                         
+ *                      |___|_|_|___|_|_|_|   
+ *                                                               
+ * ____________________________________________________________________________
+ * 
+ * CHECK PING RESPONSE TIME VIA ICMP
+ * ____________________________________________________________________________
+ * 
+ * 2022-07-05  <axel.hahn@iml.unibe.ch>
+ * 
+ */
+class checkPing extends appmonitorcheck{
+    /**
+     * get default group of this check
+     * @param array   $aParams
+     * @return array
+     */
+    public function getGroup(){
+        return 'network';
+    }
+
+    /**
+     * check ping to a target
+     * @param array $aParams
+     * array(
+     *     host                string   optional hostname to connect; default: 127.0.0.1
+     *     timeout             integer  optional timeout in sec; default: 5
+     * )
+     * @return boolean
+     */
+    public function run($aParams) {
+        $sHost = array_key_exists('host', $aParams) ? $aParams['host'] : '127.0.0.1';
+
+        if (!function_exists('socket_create')){
+            return [RESULT_UNKNOWN, "UNKNOWN: Unable to perform ping test. The socket module is not enabled in the php installation."];
+        }
+
+        /* ICMP ping packet with a pre-calculated checksum */
+        $package = "\x08\x00\x7d\x4b\x00\x00\x00\x00PingHost";
+        $socket  = socket_create(AF_INET, SOCK_RAW, 1);
+        socket_set_option(
+            $socket, 
+            SOL_SOCKET, 
+            SO_RCVTIMEO, 
+            array(
+                "sec"=>(isset($aParams["timeout"]) && (int)$aParams["timeout"]) ? (int)$aParams["timeout"] : $this->_iTimeoutTcp, // timeout in seconds
+                "usec"=>0
+              )
+        );
+
+        $start = microtime(true);
+        $connect = socket_send($socket, $package, strLen($package), 0);
+        if($connect){
+            if (socket_read($socket, 255)){
+                $result = microtime(true) - $start;
+                socket_close($socket);
+                return [RESULT_OK, 
+                    "OK: ping to $sHost in " . socket_strerror(socket_last_error($socket)),
+                    array(
+                        'type'=>'counter',
+                        'count'=>$result,
+                        'visual'=>'line',
+                    )
+
+                ];
+            } else {
+                socket_close($socket);
+                return [RESULT_ERROR, "ERROR: ping to $sHost failed after connect. " . socket_strerror(socket_last_error($socket))];
+            }
+        } else {
+            return [RESULT_ERROR, "ERROR: ping to $sHost failed. " . socket_strerror(socket_last_error($socket))];
+        }
+    }
+    
+}
