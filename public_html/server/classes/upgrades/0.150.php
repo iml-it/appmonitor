79f5e6fca2abd4ccbbbe4dcee77a2aa0fb3afe33
Hahn Axel (hahn) <axel.hahn@unibe.ch>
2025-02-25 18:43:35 +0100

    upgrader: remove cache dirs; show dots while importing notifications

--- public_html/server/classes/upgrades/0.150.php
+++ public_html/server/classes/upgrades/0.150.php
@@ -8,46 +8,49 @@ $_sCacheModule = "notificationhandler-log";
 $_sCacheId = "log";
 
 title("Switch cache data to database - import notifications");
-if($this->oNotification->countLogitems() > 0){
-    skip("Already imported notifications: ".$this->oNotification->countLogitems().". Nothing to do.");
+if ($this->oNotification->countLogitems() > 0) {
+    skip("Already imported notifications: " . $this->oNotification->countLogitems() . ". Nothing to do.");
 } else {
     // $oDB->setDebug(true);
-    if (!$_oNotifications=new objnotifications($oDB)){
+    if (!$_oNotifications = new objnotifications($oDB)) {
         fail("Failed to initilize notifications object.");
-    };
+    }
+    ;
     echo "Notifications object was initialized.\n";
 
     echo "Initializing cache...\n";
     $oCache = new AhCache($_sCacheModule, $_sCacheId);
     $aLog = $oCache->read();
-    if(!is_array($aLog) || !count($aLog)){
+    if (!is_array($aLog) || !count($aLog)) {
         skip("No notifications as cached file found.");
     } else {
 
-        echo "Found: ".count($aLog)." entries in cache\n";
+        echo "Found: " . count($aLog) . " entries in cache\n";
 
-        $iCounter=0;
-        foreach($aLog as $aLogitem){
+        $iCounter = 0;
+        foreach ($aLog as $aLogitem) {
             $iCounter++;
+            echo ".";
             // echo "$iCounter - $aLogitem[appid] - $aLogitem[message]<br>";
             $_oNotifications->new();
-    
+
             $_oNotifications->setItem([
-                'timestamp'   => $aLogitem['timestamp'],
-                'appid'       => $aLogitem['appid'],
-                'changetype'  => $aLogitem['changetype'],
-                'status'      => $aLogitem['status'],
-                'message'     => $aLogitem['message'],
+                'timestamp' => $aLogitem['timestamp'],
+                'appid' => $aLogitem['appid'],
+                'changetype' => $aLogitem['changetype'],
+                'status' => $aLogitem['status'],
+                'message' => $aLogitem['message'],
                 // 'result'      => json_encode($aLogitem['result'], 1),
             ]);
-    
-            if(!$_oNotifications->create()) {
-                fail("Error: ".$oDB->error()."<br>".print_r($oDB->lastquery(1), 1)."<br>\n");
+
+            if (!$_oNotifications->create()) {
+                fail("Error: " . $oDB->error() . "<br>" . print_r($oDB->lastquery(1), 1) . "<br>\n");
             }
         }
-    
-        echo "Notifications imported: ".$_oNotifications->count()."<br>";
-        if($_oNotifications->count() !== count($aLog)){
+        echo "\n";
+
+        echo "Notifications imported: " . $_oNotifications->count() . "<br>";
+        if ($_oNotifications->count() !== count($aLog)) {
             fail("Not all notifications were imported.");
         }
         ok();
@@ -57,13 +60,14 @@ if($this->oNotification->countLogitems() > 0){
 // ----------------------------------------------------------------------
 
 title("Import response data to database");
-if (!$_oWebapp=new objwebapps($oDB)){
+if (!$_oWebapp = new objwebapps($oDB)) {
     fail("Failed to initilize webapps object.");
-};
+}
+;
 echo "Webapps object was initialized.\n";
 
-if($_oWebapp->count()>0){
-    skip("Already imported responses: ".$_oWebapp->count().". Nothing to do.");
+if ($_oWebapp->count() > 0) {
+    skip("Already imported responses: " . $_oWebapp->count() . ". Nothing to do.");
 } else {
 
     echo "Initializing cache...\n";
@@ -71,41 +75,42 @@ if($_oWebapp->count()>0){
     $oCacheLastResult = new AhCache("notificationhandler-app"); // notificationhandler-app
 
     // $aResults=$oCacheResults->getCachedItems();
-    $aResults=$oCacheLastResult->getCachedItems();
+    $aResults = $oCacheLastResult->getCachedItems();
 
     echo "Starting import of cached responses...\n";
 
     // loop over cache entries and import
-    foreach(array_values($aResults) as $aCacheitem){
+    foreach (array_values($aResults) as $aCacheitem) {
 
         $oCacheLastResult = new AhCache("notificationhandler-app", $aCacheitem['cacheid']); // notificationhandler-app
-        $aDataLast=$oCacheLastResult->read();
+        $aDataLast = $oCacheLastResult->read();
 
         $_oWebapp->new();
         $_oWebapp->set("appid", $aCacheitem['cacheid']);
 
         $_oWebapp->set("lastresult", json_encode($aDataLast));
-        if($aDataLast['result']['result']==0){
+        if ($aDataLast['result']['result'] == 0) {
             $_oWebapp->set("lastok", json_encode($aDataLast));
         }
 
-        if(!$_oWebapp->create()) {
-            fail("Error: ".$oDB->error()."<br>".print_r($oDB->lastquery(1), 1)."<br>\n");
+        if (!$_oWebapp->create()) {
+            fail("Error: " . $oDB->error() . "<br>" . print_r($oDB->lastquery(1), 1) . "<br>\n");
         }
     }
-    ok(count($aResults)." responses imported.");
+    ok(count($aResults) . " responses imported.");
 
 }
 // ----------------------------------------------------------------------
 
 title("Import rrd data to database");
-if (!$_oSimplerrd=new objsimplerrd($oDB)){
+if (!$_oSimplerrd = new objsimplerrd($oDB)) {
     fail("Failed to initilize simplerrd object.");
-};
+}
+;
 echo "Simplerrd object was initialized.\n";
-$iCount=0;
-if($_oSimplerrd->count()>0){
-    skip("Already imported rrd data: ".$_oSimplerrd->count().". Nothing to do.");
+$iCount = 0;
+if ($_oSimplerrd->count() > 0) {
+    skip("Already imported rrd data: " . $_oSimplerrd->count() . ". Nothing to do.");
 } else {
 
     echo "Initializing cache...\n";
@@ -113,45 +118,47 @@ if($_oSimplerrd->count()>0){
     $oCacheRrd = new AhCache("rrd");
 
     // $aResults=$oCacheResults->getCachedItems();
-    $aResults=$oCacheRrd->getCachedItems();
+    $aResults = $oCacheRrd->getCachedItems();
 
     echo "Starting import of cached items...\n";
     // print_r($aResults);
     // loop over cache entries and import
-    foreach(array_values($aResults) as $aCacheitem){
+    foreach (array_values($aResults) as $aCacheitem) {
         // $oCacheResults->setCacheId($aCacheitem['cacheid']);
         // $aData=$oCacheResults->read();
         // print_r($aData);
         $iCount++;
 
-        $sAppid=substr($aCacheitem['cacheid'], 0, 32);
-        $sCounterkey=substr($aCacheitem['cacheid'], 33, 200);
+        $sAppid = substr($aCacheitem['cacheid'], 0, 32);
+        $sCounterkey = substr($aCacheitem['cacheid'], 33, 200);
 
 
         $oCacheRrd = new AhCache("rrd", $aCacheitem['cacheid']);
         $oCacheRrd->setCacheId($aCacheitem['cacheid']);
-        $aDataCache=$oCacheRrd->read();
-        echo "$sAppid - $sCounterkey - ".count($aDataCache)." values .. ";
+        $aDataCache = $oCacheRrd->read();
+        echo "$sAppid - $sCounterkey - " . count($aDataCache) . " values .. ";
 
         // print_r($aDataCache); die();
         $_oSimplerrd->new();
 
-        if (!$_oSimplerrd->setItem([
-            "appid" => $sAppid,
-            "countername" => $sCounterkey,
-            "data" => json_encode($aDataCache),
-            // "timestamp" => $aDataset['timestamp'],
-            // "data" => $aDataset['data']['status'],
-            // "value" => $aDataset['data']['value']
-        ])){
+        if (
+            !$_oSimplerrd->setItem([
+                "appid" => $sAppid,
+                "countername" => $sCounterkey,
+                "data" => json_encode($aDataCache),
+                // "timestamp" => $aDataset['timestamp'],
+                // "data" => $aDataset['data']['status'],
+                // "value" => $aDataset['data']['value']
+            ])
+        ) {
             fail("Failed to set item #$iCount :-/");
         }
         echo "OK.\n";
 
-        if(!$_oSimplerrd->create()) {
-            fail("Error: ".$oDB->error()."<br>".print_r($oDB->lastquery(1), 1)."<br>\n");
+        if (!$_oSimplerrd->create()) {
+            fail("Error: " . $oDB->error() . "<br>" . print_r($oDB->lastquery(1), 1) . "<br>\n");
         }
-        
+
     }
     ok("$iCount rrd items imported.");
 
@@ -159,26 +166,14 @@ if($_oSimplerrd->count()>0){
 
 // ----------------------------------------------------------------------
 
-foreach([
-    "notificationhandler-app",
-    "notificationhandler-log",
-    "notificationhandler-notify",
-    "responsetime",
-    "rrd"
-] as $sModulename){
-    
-    title("Drop old cache - $sModulename");
+foreach (["counterids", "notificationhandler-app", "notificationhandler-log", "notificationhandler-notify", "responsetime", "rrd"] as $sModulename) {
     $oCache = new AhCache($sModulename);
-    $iCachefiles=count($oCache->getCachedItems());
-    if ($iCachefiles){
-        echo "Deleting data...\n";
-        if ($oCache->deleteModule()){
-            ok();
-        } else {
-            fail();
-        }
+    $iCachefiles = count($oCache->getCachedItems());
+    title("Drop old cache - $sModulename ($iCachefiles items)");
+    if ($oCache->deleteModule()) {
+        ok();
     } else {
-        skip("No cache found.");
+        fail();
     }
 }
 
