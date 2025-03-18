cf6dc58bdbdb32028db03db248695f04f9c97c15
Hahn Axel (hahn) <axel.hahn@unibe.ch>
2025-03-18 17:14:44 +0100

    add first version for an abstract validation class

---
+++ public_html/client/classes/validateparam.class.php
@@ -0,0 +1,147 @@
+<?php
+class validateparam {
+
+    protected array $_aRegexDefs=[
+        'website' => '/https?:\/\//'
+    ];
+
+    protected bool $_flag_debug = false;
+
+
+    protected function _wd($s)
+    {
+        if($this->_flag_debug){
+            echo "DEBUG: $s<br>\n";
+        }
+    }
+
+    /**
+     * Validate a single value. It returns a string with an errormessage.
+     * No error means ok.
+     * 
+     * @param array $aOpt  Validation rules
+     * @param mixed $value value to verify
+     * @return string with found error
+     */
+    public function validateValue($aOpt, $value){
+        $sError='';
+
+        // check type
+        if(isset($aOpt['type'])){
+            $this->_wd("check type $aOpt[type]");
+            switch ($aOpt['type']){
+                case 'array':
+                    if (!is_array($value)){
+                        $sError.="Value isn't an array";
+                    }
+                    break;
+                case 'bool':
+                    if (!is_bool($value)){
+                        $sError.="Value '$value' isn't a bool";
+                    }
+                    break;
+                case 'float':
+                    if (!is_float($value)){
+                        $sError.="Value isn't a float";
+                    }
+                    break;
+    
+                case 'int':
+                case 'integer':
+                    if (!is_int($value)){
+                        $sError.="Value '$value' isn't an integer";
+                    } else {
+                        if($aOpt['min']??false){
+                            if($value<$aOpt['min']){
+                                $sError.="Value is too small; minimum is $aOpt[min]";
+                            }
+                        }
+                        if($aOpt['max']??false){
+                            if($value>$aOpt['max']){
+                                $sError.="Value is too big; maximum is $aOpt[max]";
+                            }
+                        }
+                    }
+                    break;
+    
+                case 'string':
+                    if (!is_string($value) ){
+                        $sError.="Value '$value' isn't a string";
+                    } else {
+                        if($aOpt['regex']??false){
+                            if(!preg_match($aOpt['regex'], $value)){
+                                $sError.="Value is invalid. Regex doesn't match: $aOpt[regex]";
+                            }
+                        }
+                        if(isset($aOpt['oneof']) && is_array($aOpt['oneof'])){
+                            if(array_search($value, $aOpt['oneof'])==false){
+                                $sError.="Value is invalid. Value doesn't match one of these values ".print_r($aOpt['oneof']);
+                            }
+                        }
+                    }
+                    break;
+
+                default:
+                    echo "SKIP valdation - unknown type: $aOpt[type]<br>\n";
+            }
+        }
+        // if string: verify regex
+
+        return $sError;
+    }
+
+    /**
+     * Validate an array of parameter definitions. It returns an array of all error messages
+     * No error / an empty array means ok.
+     * 
+     * @param mixed $aDefs    array with definitions
+     * @param mixed $aParams  array of given values
+     * @param mixed $bStrict  flag: strict checking to detect wrong keys.
+     * @return array with errors
+     */
+    public function validateArray($aDefs, $aParams, $bStrict = true)
+    {
+        $aErrors=[];
+        // echo "<pre>";
+        // print_r($aParams);
+        // print_r($aDefs); 
+        // echo "<hr>";
+
+        $aTmp=$aParams;
+        foreach($aDefs as $sKey => $aOpt){
+            if ($aOpt['required']??false){
+
+                $this->_wd("Check MUST $sKey");
+                // verify MUST field
+                if(!isset($aParams[$sKey])){
+                    $aErrors[$sKey]="Missing required key '$sKey'";
+                } else {
+                    $this->_wd("MUST field exists: $sKey");
+                    $sError=$this->validateValue($aOpt, $aParams[$sKey]);
+                    if($sError){
+                        $aErrors[$sKey]=$sError;
+                    } else {
+                        $this->_wd("$sKey was successfully validated.<hr>");
+                    }
+                    unset($aTmp[$sKey]);
+                }
+            }
+            if(isset($aOpt['required']) && !$aOpt['required'] && isset($aParams[$sKey])){
+                $this->_wd("Check OPTIONAL $sKey");
+                $sError=$this->validateValue($aOpt, $aParams[$sKey]);
+                if($sError){
+                    $aErrors[$sKey]=$sError;
+                }
+                unset($aTmp[$sKey]);
+            }
+        }
+        if($bStrict && count($aTmp)){
+            foreach(array_keys($aTmp) as $sKey)
+            {
+                $aErrors[$sKey]="Invalid key was found: '$sKey'; allowed keys are '".implode("', '", array_keys($aDefs))."'";
+            }
+        }
+        $this->_wd("found errors: " . print_r($aErrors, 1));
+        return $aErrors;
+    }
+}
---
+++ public_html/client/classes/validateparam.class.php.md
@@ -0,0 +1,86 @@
+# validateparam.class.php
+
+## Description
+
+This class can validate values and arrays.
+You define type and validateion rules. A validation method checks the given value against your rules. It returns the error message(s). No error means, the value is ok.
+
+## Usage
+
+```php
+
+$oVal=new validateparam();
+
+// check an array
+$aErrors=$oVal->validateArray($aRules, $aGivenValues, $bStrict);
+if(count($aErrors)){
+    echo "Something is wrong: <pre>".print_r($aErrors, 1)."</pre>";
+    exit(1);
+}
+```
+
+## Validate Arrays
+
+First something visual - an example:
+
+```txt
+Array
+(
+    [name] => Array
+        (
+            [type] => string
+            [required] => true
+            [regex] => /./
+        )
+
+    [description] => Array
+        (
+            [type] => string
+            [required] => true
+            [regex] => /./
+        )
+        :
+    [worstresult] => Array
+        (
+            'type' => 'int',
+            'required' => false,
+            'description' => 'A failed check is max counted as given result. Use it on not required but optional checks',
+            'min'=>RESULT_OK,
+            'max'=>RESULT_ERROR,
+        )
+)
+```
+
+### Type check
+
+Each key can be marked to be a value of a given type.
+
+It can be a mandantory value or optional.
+
+| Name       | Type       | Description
+|--          |--          |--
+| 'type'     | {string}   | variable type that must match; one of "array", "bool", "float", "int", "integer", "string"
+| 'required' | {bool}     | define value as required
+
+Next to these keys per type you can define validation rules in dependency of the type.
+
+### Validate numbers
+
+Values of the types "float" and "int"/ "integer" you can verify if it is in awanted range. Both values are optional.
+
+| Name       | Type         | Description
+|--          |--            |--
+| 'min'      | {float\|int} | allowed minimum value
+| 'max'      | {float\|int} | allowed maximum value
+
+### Validate string
+
+Values of type "string" can be verified
+
+* against a given regex
+* with a set of allowed values
+
+| Name       | Type       | Description
+|--          |--          |--
+| 'regex'    | {string}   | value must match given regex
+| 'oneof'    | {array}    | value must match one of the given values
