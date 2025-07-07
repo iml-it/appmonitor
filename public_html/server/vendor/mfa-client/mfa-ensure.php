ef2aa2ccddecaec76b65cb8df1c3117a3b0e23da
Hahn Axel (hahn) <axel.hahn@unibe.ch>
2025-07-07 11:04:51 +0200

    update mfa client

--- public_html/server/vendor/mfa-client/mfa-ensure.php
+++ public_html/server/vendor/mfa-client/mfa-ensure.php
@@ -1,15 +1,17 @@
 <?php
 /**
  * mfa-ensure.php
- * included by public_html/server/classes/appmonitor-server-gui.class.php
  * 
- * @author Axel Hahn <axel.hahn@unibe>
+ * @author Axel Hahn <axel.hahn@unibe.ch>
  * 
  */
 
 require_once __DIR__.'/mfaclient.class.php';
 $mfa = new mfaclient();
 
+$mfa->debug($aConfig['debug']??false);
+
+// if user was not set in config, set it manually
 $mfa->setUser($this->getUserid());
 
 $iHttpStatus=$mfa->ensure();
