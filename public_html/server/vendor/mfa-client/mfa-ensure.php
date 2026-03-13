576dc5e4c9c2c896ecd7fa8adfb9d4b5327672eb
Hahn Axel (hahn) <axel.hahn@unibe.ch>
2026-03-13 15:14:36 +0100

    fix mfa-ensure

--- public_html/server/vendor/mfa-client/mfa-ensure.php
+++ public_html/server/vendor/mfa-client/mfa-ensure.php
@@ -9,7 +9,7 @@
 require_once __DIR__.'/mfaclient.class.php';
 $mfa = new mfaclient();
 
-$mfa->debug((bool) $aConfig['debug']??false);
+$mfa->debug((bool) ($aConfig['debug']??false));
 
 // if user was not set in config, set it manually
 // $mfa->setUser(<your-function-to-fetch-userid>);
